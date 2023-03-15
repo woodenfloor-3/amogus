02ea7644d90397e83c4aaade4d61c17b607f28cf
WOODENFLOORSUS <127944520+WOODENFLOORSUS@users.noreply.github.com>
2023-03-15 17:38:04 +0530

    Revert "Add files via upload"
    This reverts commit 579ac6a40207425ab73cca72e7f8ba189a029f21.

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-echo "Logging You Out Please Wait......";
-session_destroy();
-header("location: index.php?logout=true");
-?>
--- partials/_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$database = "sus";
-
-$conn = mysqli_connect($servername, $username, $password, $database);
-?>
