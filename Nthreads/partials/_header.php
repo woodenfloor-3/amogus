973a3b0380137f8d3766fd597807cdfe5a41fc91
WOODENFLOORSUS <127944520+WOODENFLOORSUS@users.noreply.github.com>
2023-03-15 20:33:25 +0530

    cahnge cat limit
    cahnged cat limit to 5 for nav bar dropdown

--- Nthreads/partials/_header.php
+++ Nthreads/partials/_header.php
@@ -87,7 +87,7 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
       </a>
       <div class="dropdown-menu" aria-labelledby="navbarDropdown">';
 
-        $sql = "SELECT category_name, category_id FROM categories LIMIT 3";
+        $sql = "SELECT category_name, category_id FROM categories LIMIT 5";
         $result = mysqli_query($conn, $sql);
         while($row = mysqli_fetch_assoc($result)){
           echo '<a class="dropdown-item" href="threadlist.php?catid='.$row["category_id"].'">' . $row["category_name"] . '</a>';
