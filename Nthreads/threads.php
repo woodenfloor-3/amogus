7eab63933b88b454aa44912a420a403449b8a905
WOODENFLOORSUS <127944520+WOODENFLOORSUS@users.noreply.github.com>
2023-03-20 12:50:17 +0530

    dda

--- Nthreads/threads.php
+++ Nthreads/threads.php
@@ -151,7 +151,7 @@ function copyToClipboard() {
           $noResult = false;
           $comment_by = $row['comment_by'];
 
-          $sql2 = "SELECT user_email FROM users WHERE srn = $comment_by";
+          $sql2 = "SELECT user_email, user_image FROM users WHERE srn = $comment_by";
           $result2 = mysqli_query($conn, $sql2);
           $row2 = mysqli_fetch_assoc($result2);
       
