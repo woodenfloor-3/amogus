20baf191b4f66c91d0553ef06e1e85b91782238f
WOODENFLOORSUS <127944520+WOODENFLOORSUS@users.noreply.github.com>
2023-03-21 21:31:49 +0530

    Update sus.sql

--- Nthreads/database/sus.sql
+++ Nthreads/database/sus.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 19, 2023 at 03:55 PM
+-- Generation Time: Mar 21, 2023 at 05:01 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -29,8 +29,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `category_id` int(8) NOT NULL,
-  `category_name` varchar(255) NOT NULL,
-  `category_description` varchar(255) NOT NULL,
+  `category_name` varchar(600) NOT NULL,
+  `category_description` varchar(600) NOT NULL,
   `created` datetime NOT NULL,
   `image_path` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -40,24 +40,12 @@ CREATE TABLE `categories` (
 --
 
 INSERT INTO `categories` (`category_id`, `category_name`, `category_description`, `created`, `image_path`) VALUES
-(1, 'MCA', 'MCA FORUM', '2022-05-25 08:52:14', ''),
-(2, 'MMS', 'MMS FORUM', '2022-05-25 08:54:22', ''),
-(3, 'NIL TEACHING SESSION', 'THIS IS WHERE NILADRI SAHA TEACHES THE STUDENTS', '2023-03-15 13:09:12', ''),
-(4, 'ARIF TEACHING ', 'TEACHER', '2023-03-15 13:10:13', ''),
-(5, 'dd', 'aaa', '0000-00-00 00:00:00', ''),
-(6, 'htis testsd', 'dasdas', '0000-00-00 00:00:00', ''),
-(8, 'Category Name', 'Category Description', '0000-00-00 00:00:00', '/uploads/'),
-(29, 'y', 'y', '0000-00-00 00:00:00', ''),
-(30, 'sas', 'as', '0000-00-00 00:00:00', ''),
-(32, 'ss', 'asd', '0000-00-00 00:00:00', 'uploads/Untitled.png'),
-(33, 'new img ', 'sess', '0000-00-00 00:00:00', 'uploads/Screenshot_2022.10.21_20.56.54.680.png'),
-(34, 'arknights', 'arknights', '0000-00-00 00:00:00', 'uploads/Screenshot_2022.11.06_14.44.57.970.png'),
-(35, 'essex', 'best shipfu', '0000-00-00 00:00:00', 'uploads/ess_azurlane230209.gif'),
-(36, 'gi', 'dehya', '0000-00-00 00:00:00', 'uploads/dhy_genshin230301.gif'),
-(37, 'arknights', 'suzuran', '0000-00-00 00:00:00', 'uploads/szr_arknights230306.gif'),
-(38, 'ss', 'ganyu', '2023-03-19 18:14:57', 'uploads/99d43befb3df2d6293974cfc7313d7e0.gif'),
-(39, 'ss', 'ganyu', '2023-03-19 18:16:23', 'uploads/99d43befb3df2d6293974cfc7313d7e0.gif'),
-(40, 'gggg', 'ggg', '2023-03-19 18:19:55', 'uploads/pwrgzplk5ad71.gif');
+(43, 'hgcvcghf', 'rdfzx', '2023-03-20 12:46:22', 'uploads/userimage.png'),
+(44, 'paimon as a food source', 'lumine on her way to use paimon as emergency food', '2023-03-21 20:08:29', 'uploads/1114887.jpg'),
+(47, 'Ganyu Build', 'Ganyu has a general artifact set recommendation, which is the Wanderer\'s Troupe. However there are other options for her. In theory, you can mix and match any of these 3 artifact sets as long as you get 2 sets of 2 pieces.', '2023-03-21 20:49:12', 'uploads/1130246.jpg'),
+(48, 'Hu tao', 'Hu Tao is a strong Pyro carry, capable of continuous Pyro application with her Elemental Skill. She does have a high risk, high reward gameplay so she does have her drawbacks, but overall, she\'s one of the top Pyro characters in the current meta.', '2023-03-21 20:49:52', 'uploads/1129086.png'),
+(49, 'seseran gif', 'seseran \r\nhttps://twitter.com/Seseren_kr', '2023-03-21 20:52:16', 'uploads/szr_arknights230306.gif'),
+(51, 'Dorothy\'s Vision', 'Dorothy\'s Vision expands the story of Rhine Lab for the first time, featuring the eponymous character conducting an ethically questionable science experiment that will soon go haywire and the behind-the-scenes actions among R.L.\'s superiors such as political involvement by the Columbian military.', '2023-03-21 21:16:23', 'uploads/425b8a851b6de1d32e54a8c55776fa7a.png');
 
 -- --------------------------------------------------------
 
@@ -78,12 +66,31 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`comment_id`, `comment_content`, `thread_id`, `comment_time`, `comment_by`) VALUES
-(1, 'Hello\r\nThis is Comment', 1, '2022-05-30 07:37:43', 28),
-(22, 'jQuery is a library', 104, '2022-06-19 15:11:34', 44),
-(23, 'jQuery is a library not a framework', 104, '2022-06-19 15:12:37', 44),
-(24, 'answer bta to bs\r\n', 106, '2022-06-20 10:22:23', 44),
-(25, 'answer bta to bs\r\n', 106, '2022-06-20 10:33:34', 44),
-(26, '&lt;alert&gt;Hello&lt;/alert&gt;', 106, '2022-06-20 11:18:54', 47);
+(1, 'jyhgfcxj vnb', 117, '2023-03-20 12:48:05', 132),
+(2, 'gc bhnm j', 117, '2023-03-20 12:48:29', 130),
+(3, 'gc bhnm j', 117, '2023-03-20 12:49:37', 130);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE `images` (
+  `image_path` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `images`
+--
+
+INSERT INTO `images` (`image_path`) VALUES
+('announcements/1123013.jpg'),
+('announcements/ganyu.jpg'),
+('announcements/wp10165064.png'),
+('announcements/wp7487214.jpg'),
+('announcements/wp9660709.jpg'),
+('announcements/wp9999057.jpg');
 
 -- --------------------------------------------------------
 
@@ -105,11 +112,7 @@ CREATE TABLE `threads` (
 --
 
 INSERT INTO `threads` (`thread_id`, `thread_title`, `thread_desc`, `thread_cat_id`, `thread_user_id`, `timestamp`) VALUES
-(1, 'Unable to use Python in MAC OS', 'Hello\r\nI have MAC Book and want to learn Python but the problem is, My laptop shows an error when I tried to execute the programs.', 1, 28, '2022-05-27 08:57:41'),
-(10, 'demo for javascript', 'description', 2, 38, '2022-05-29 22:42:37'),
-(94, 'this is question', 'description', 3, 45, '2022-06-19 10:17:35'),
-(105, 'testing', 'description', 2, 44, '2022-06-19 17:35:01'),
-(108, 'Hello Help me', 'No', 1, 41, '2023-03-15 16:34:47');
+(118, 'based', 'based', 44, 1, '2023-03-21 21:22:32');
 
 -- --------------------------------------------------------
 
@@ -122,38 +125,20 @@ CREATE TABLE `users` (
   `user_email` varchar(20) NOT NULL,
   `user_password` varchar(20) NOT NULL,
   `timestamp` datetime NOT NULL,
-  `is_admin` tinyint(1) NOT NULL DEFAULT 0
+  `is_admin` tinyint(1) NOT NULL DEFAULT 0,
+  `user_image` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`srn`, `user_email`, `user_password`, `timestamp`, `is_admin`) VALUES
-(28, 'nbarshan2000@gmail.c', '$2y$10$aC95z1qOZ65re', '2022-06-03 07:50:05', 0),
-(29, 'abc@aa.in', '$2y$10$I6nAR8AdCWb.H', '2022-06-03 08:01:41', 0),
-(30, 'rohan@asl.ljk', '$2y$10$cx0HDJPh.0ma3', '2022-06-03 08:07:22', 0),
-(31, 'exp@api.in', '$2y$10$Xp90Fndnxg2k8', '2022-06-03 11:23:10', 0),
-(32, 'abc@gmail.com', '$2y$10$DRi0UCFQdnsyl', '2022-06-03 12:04:55', 0),
-(33, 'am@gmail.com', '$2y$10$Zj6Sliz359/Ub', '2022-06-03 12:32:20', 0),
-(34, 'hello@world.in', '123', '2022-06-03 12:41:36', 0),
-(35, 'good@morning.in', 'jj', '2022-06-04 07:49:09', 0),
-(38, 'neeraj@mail.in', '111', '2022-06-06 12:25:38', 0),
-(40, 'good@night.in', '111', '2022-06-06 22:58:37', 0),
-(41, 'rahul@com.com', '1234', '2022-06-06 23:18:49', 0),
-(42, 'hello@mail.in', '111', '2022-06-11 22:57:18', 0),
-(43, 'demo@abc.in', '111', '2022-06-12 15:16:48', 0),
-(44, 'abc1234@gmail.com', '1234', '2022-06-18 10:53:01', 0),
-(45, 'neeraj@abc.in', '111', '2022-06-19 10:16:28', 0),
-(46, 'JAIRAMCM@GMAIL.COM', 'JAIRAM123', '2022-06-20 10:17:32', 0),
-(47, 'neeraj', '111', '2022-06-20 10:34:11', 0),
-(99, 'NIL', 'NIL', '2023-03-19 08:29:39', 0),
-(100, 'admin', 'admin', '2023-03-19 14:24:08', 1),
-(101, 'adduser', 'adduser', '2023-03-19 14:53:40', 0),
-(102, 'woodenfloortestadmin', '123', '2023-03-19 14:59:33', 0),
-(103, 'woodenfloorsdsadfas', '123', '2023-03-19 15:44:21', 0),
-(104, 'woodenfloor', '123', '2023-03-19 18:09:20', 0),
-(105, 'woodenfloorssada', '123', '2023-03-19 18:18:35', 0);
+INSERT INTO `users` (`srn`, `user_email`, `user_password`, `timestamp`, `is_admin`, `user_image`) VALUES
+(1, 'woodenfloor', '123', '2023-03-20 09:53:04', 1, 'userimgs/userimage.png'),
+(129, 'user1', '12345678', '2023-03-20 09:54:19', 0, 'userimgs/eula.png'),
+(130, 'user2', '12345', '2023-03-20 09:54:31', 0, 'userimgs/ganyu.png'),
+(131, 'user3', '1234fasg', '2023-03-20 09:55:00', 0, 'userimgs/keqing.png'),
+(132, 'wow', '123', '2023-03-20 12:47:43', 0, 'userimgs/Screenshot 2022-04-11 001750.png');
 
 --
 -- Indexes for dumped tables
@@ -170,6 +155,12 @@ ALTER TABLE `categories`
 --
 ALTER TABLE `comments`
   ADD PRIMARY KEY (`comment_id`);
+
+--
+-- Indexes for table `images`
+--
+ALTER TABLE `images`
+  ADD UNIQUE KEY `image_path` (`image_path`);
 
 --
 -- Indexes for table `threads`
@@ -192,25 +183,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+  MODIFY `category_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
 
 --
 -- AUTO_INCREMENT for table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `comment_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `comment_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `threads`
 --
 ALTER TABLE `threads`
-  MODIFY `thread_id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+  MODIFY `thread_id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=119;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `srn` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=106;
+  MODIFY `srn` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=133;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
